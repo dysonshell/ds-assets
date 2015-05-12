5db98008acf7390f8fbbff3012d955e7ab04711c
Anson Yu Wang <opensource@undozen.com>
2015-05-12 15:13:08 +0800

    make sed -i compatible on both mac and linux

--- compile-less.sh
+++ compile-less.sh
@@ -4,4 +4,5 @@ FILEBASE="$( basename -s .less "$1" )"
 CSSFILE="$DIR/${FILEBASE}.css"
 lessc $1 --source-map="${CSSFILE}.map" --source-map-url="/${CSSFILE}.map" --source-map-basepath=$(pwd) --source-map-rootpath=/ > "$DIR/${FILEBASE}.css" > $CSSFILE
 REGREP='s#"file":"[^"]*undefined"#"file":"/'"$CSSFILE"'\"#g'
-sed -i $REGREP ${CSSFILE}.map
+sed -i.bak $REGREP ${CSSFILE}.map
+rm -rf ${CSSFILE}.map.bak
